de28d117237d0de6f6efb056850e93d832f2f1b5
Nguyễn Vũ Khoa <150508398+Nvkhoaaa@users.noreply.github.com>
2026-04-03 21:53:45 +0700

    Khoa update project

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+import forms from "@tailwindcss/forms";
+
+module.exports = {
+  darkMode: "class",
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [forms],
+};
